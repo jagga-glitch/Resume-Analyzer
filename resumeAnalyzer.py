44ad8ec9acb9d2a5426446cd36647fcbc1f88cb1
Jigyasa <guptajigyasa893@gmail.com>
2026-07-17 11:31:13 +0530

    added streamlit secrets

--- resumeAnalyzer.py
+++ resumeAnalyzer.py
@@ -17,7 +17,7 @@ load_dotenv()
 
 my_api_key = os.getenv("GROQ_API_KEY")
 if not my_api_key:
-    raise ValueError("GROQ_API_KEY environment variable is not set.")
+    my_api_key = st.secrets.get("GROQ_API_KEY")
 
 client = Groq(api_key=my_api_key)
 model = "llama-3.3-70b-versatile"
